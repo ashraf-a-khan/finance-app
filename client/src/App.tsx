a3293c9e49551a10c287fbc9a537dd4acee19e12
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-05-08 00:00:47 -0400

    Add layout for boxes in grids

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
     const theme = useMemo(() => createTheme(themeSettings), []);
@@ -19,10 +20,7 @@ function App() {
                     >
                         <Navbar />
                         <Routes>
-                            <Route
-                                path="/"
-                                element={<div>dashboard page</div>}
-                            />
+                            <Route path="/" element={<Dashboard />} />
                             <Route
                                 path="/predictions"
                                 element={<div>predictions page</div>}
